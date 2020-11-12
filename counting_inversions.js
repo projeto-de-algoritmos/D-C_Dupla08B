5e784db42af70465faa7d471b9c4ddeddb517ec1
Arthur <arthurarp@hotmail.com>
2020-11-12 17:46:35 -0300

    mudanças na estrutura

--- counting_inversions.js
+++ counting_inversions.js
@@ -1,19 +1,40 @@
 class CountingInversions {
-
-    constructor (a, b) {
-
-        var vector_a = a;
-        var vector_b = b;
-        var vector_aux = [];
-
-    }
     
     divide_list(list) {  // O(1)
 
     }
 
-
     sort_and_count(list) {
+        let total = 0;
+
+        if(list.length == 0)
+            return {
+                first: 0,
+                second: list
+            };
+
+        const { left, right } = this.divide_list(list);
+
+        const { r_left, A } = this.sort_and_count(left);
+        const { r_right, B } = this.sort_and_count(left);
+        const { r, L } = this.merge_and_count(A, B);
+
+        return {
+            first: r = r_left + r_right + r,
+            second: L
+        };
+    }
+
+    merge_and_count(list_a, list_b) {
+        let aux = [];
+        let inversions = 0;
+
+        
+
 
     }
 }
+
+const ci = new CountingInversions();
+const { first, second } = ci.sort_and_count([]);
+console.log(first);
