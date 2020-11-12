4de969c46ee39c466a8d20da5fa9179028e15007
Arthur <arthurarp@hotmail.com>
2020-11-11 22:22:02 -0300

    Adicionando estrutura inicial da classe CountingInversions

---
+++ counting_inversions.js
@@ -0,0 +1,19 @@
+class CountingInversions {
+
+    constructor (a, b) {
+
+        var vector_a = a;
+        var vector_b = b;
+        var vector_aux = [];
+
+    }
+    
+    divide_list(list) {  // O(1)
+
+    }
+
+
+    sort_and_count(list) {
+
+    }
+}
