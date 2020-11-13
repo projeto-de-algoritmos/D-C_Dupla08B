0c8d6c930a9b6317ba9de7ddb60251c96c17db09
Arthur <arthurarp@hotmail.com>
2020-11-12 23:07:06 -0300

    implementando o merge and sort

--- counting_inversions.js
+++ counting_inversions.js
@@ -28,8 +28,41 @@ class CountingInversions {
     merge_and_count(list_a, list_b) {
         let aux = [];
         let inversions = 0;
-
+        let index_a = list_a.length - 1;
+        let index_b = 0;
         
+        for(let i = 0; i <= list_a.length && index_b < list_b.length; ++i) {
+            console.log('A index aqui ====> ', i)
+            console.log('B index aqui ====> ', index_b)
+
+            if(i == list_a.length && index_b < list_b.length) {
+                aux.push(list_b[index_b++]);
+                --i;
+            }
+           
+            else if(list_a[i] > list_b[index_b]) {
+                console.log(list_a[i] + ' => ' + list_b[index_b])
+                inversions += (list_a.length - i);
+                aux.push(list_b[index_b++]);
+                console.log(inversions);
+                console.log(aux)
+                --i;
+            }
+            else {
+                console.log(list_a[i] + ' => ' + list_b[index_b])
+                aux.push(list_a[i]);
+                console.log(aux)
+            }
+            
+        }
+
+
+        return {
+            inversions: inversions,
+            list: aux
+        }
+
+
 
 
     }
@@ -37,4 +70,6 @@ class CountingInversions {
 
 const ci = new CountingInversions();
 const { first, second } = ci.sort_and_count([]);
-console.log(first);
+const result = ci.merge_and_count([3, 7, 10, 14, 18, 19], [2, 11, 16, 17, 23, 25]);
+
+console.log(result);
